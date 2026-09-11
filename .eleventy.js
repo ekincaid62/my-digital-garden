466522e0d27f37b488bcf9f07d0ff8f5db7437f9
ekincaid62 <58613679+ekincaid62@users.noreply.github.com>
2026-09-10 21:22:02 -0400

    Add filter to sort blog posts by date

--- .eleventy.js
+++ .eleventy.js
@@ -469,6 +469,18 @@ module.exports = function(eleventyConfig) {
     return date && date.toISOString();
   });
 
+    eleventyConfig.addFilter("sortBlogPosts", function(posts) {
+    return [...posts].sort((a, b) => {
+      const dateA = a.data?.["dg-note-properties"]?.date;
+      const dateB = b.data?.["dg-note-properties"]?.date;
+
+      if (!dateA && !dateB) return 0;
+      if (!dateA) return 1;
+      if (!dateB) return -1;
+
+      return new Date(dateB) - new Date(dateA);
+    });
+  });
   eleventyConfig.addFilter("link", function(str) {
     return (
       str &&
