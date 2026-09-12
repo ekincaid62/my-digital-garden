fa0d2c1e85e5920767001b9790db0bb511df1967
ekincaid62 <58613679+ekincaid62@users.noreply.github.com>
2026-09-12 13:19:45 -0400

    Import userBlogImages in userEleventySetup function

--- src/helpers/userSetup.js
+++ src/helpers/userSetup.js
@@ -1,10 +1,16 @@
+const { userBlogImages } = require("./userBlogImages");
+
 function userMarkdownSetup(md) {
   // The md parameter stands for the markdown-it instance used throughout the site generator.
   // Feel free to add any plugin you want here instead of /.eleventy.js
 }
+
 function userEleventySetup(eleventyConfig) {
-  // The eleventyConfig parameter stands for the the config instantiated in /.eleventy.js.
+  // The eleventyConfig parameter stands for the config instantiated in /.eleventy.js.
   // Feel free to add any plugin you want here instead of /.eleventy.js
+
+  userBlogImages(eleventyConfig);
 }
+
 exports.userMarkdownSetup = userMarkdownSetup;
 exports.userEleventySetup = userEleventySetup;
